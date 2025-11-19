e0c9f5585f4e2a16a83270e4cd3ca50e5b8e0308
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2025-11-19 15:02:18 -0500

    Interacción usuario e IA

--- scripts/dialog_ui.gd
+++ scripts/dialog_ui.gd
@@ -1,3 +1,7 @@
 extends Control
 @onready var dialog_line = %DialogLine
 @onready var speaker_name = %SpeakerName
+@onready var text_edit: TextEdit = $DialogBox/TextEdit
+
+func _ready()->void:
+	text_edit.grab_focus()
--- scripts/mainScene.gd
+++ scripts/mainScene.gd
@@ -1,53 +1,77 @@
 extends Control
 
 #Importaciones
-@onready var http_request: HTTPRequest = $Button/HTTPRequest
+@onready var http_request: HTTPRequest = $HTTPRequest
 @onready var rich_text_label: RichTextLabel = $RichTextLabel
 @onready var character: Node2D = %Character
 @onready var dialog_ui: Control = %DialogUI
 
 ##Variables
+enum STATE {AI, HUMAN}
+var conversationTurn= STATE.HUMAN
 const  URL_API= "http://localhost:5000/api/chat"
+const ASSISTANT_NAME= "SophIA"
 var dialog_index:int=0
-var question= "¿Quién es el coordinador de la carrera?"
+var question= ""
 var dialog_lines:Array[String]=[
-	"SophIA: Hola, bienvenido a la feria","SophIA: Espero que tu estadía aquí sea acogedora"
+	
 ]
 
 
-func parse_line(line:String):
-	var line_info= line.split(":")
-	assert(len(line_info)>=2)
-	return {
-		"speaker_name": line_info[0],
-		"dialog_line": line_info[1]
-	}
+
 
 func process_current_line():
 	if(len(dialog_lines)>0):
 		var line= dialog_lines[dialog_index]
-		var line_info= parse_line(line)
-		dialog_ui.speaker_name.text=line_info["speaker_name"]
-		dialog_ui.dialog_line.text=line_info["dialog_line"]
+		dialog_ui.speaker_name.text=ASSISTANT_NAME
+		dialog_ui.dialog_line.text=line
 	 
-
+func process_human_dialog():
+	conversationTurn=STATE.AI
+	question= dialog_ui.text_edit.text
+	dialog_ui.text_edit.text=""
+	dialog_ui.text_edit.visible=false
+	dialog_ui.dialog_line.visible=true
+	dialog_ui.speaker_name.text="SophIA"
+	dialog_ui.dialog_line.text="Estoy procesando tu pregunta..."
+	callApi()
 
 
 
 func _ready() -> void:
 	dialog_index=0
+	dialog_ui.speaker_name.text="Tú"
 	process_current_line()
 
 func _input(event):
 	if (event.is_action_pressed("next_line")):
-		if dialog_index< len(dialog_lines)-1:
-			dialog_index+=1
-			process_current_line()
+		if(conversationTurn==STATE.HUMAN):
+			process_human_dialog()
+		if(conversationTurn==STATE.AI):
+			if dialog_index== len (dialog_lines)-1 and len(dialog_lines)>0:
+				process_current_line()
+				dialog_index=0
+				dialog_lines=[]
+				dialog_ui.speaker_name.text="Tú"
+				dialog_ui.text_edit.visible=true
+				dialog_ui.dialog_line.visible=false
+				conversationTurn=STATE.HUMAN
+				await get_tree().create_timer(0.25).timeout
+				dialog_ui.text_edit.grab_focus()
+				
+			elif dialog_index< len(dialog_lines)-1:
+				dialog_index+=1
+				process_current_line()
+			
 
 
 
 
-func _on_button_pressed() -> void:
+
+
+	
+	
+func callApi()->void:
 	print("Pregunta enviada")
 	var headers = ["Content-Type: application/json"]
 	var data= {"question":question}
@@ -59,4 +83,7 @@ func _on_button_pressed() -> void:
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	var data= JSON.parse_string(body.get_string_from_utf8())
 	print(data.answer)
+	var answer=	data.answer.split("\n\n")
+	dialog_lines= Array( Array(answer), TYPE_STRING, "", null )
+	process_current_line()
 	pass # Replace with function body.
