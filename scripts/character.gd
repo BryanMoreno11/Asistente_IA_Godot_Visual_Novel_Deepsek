f4ad6e1af141a9cd4625adf26ffe25b2f70c6fcb
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2025-11-22 14:36:35 -0500

    Nuevas animaciones de respuesta

--- scripts/character.gd
+++ scripts/character.gd
@@ -2,9 +2,13 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 #variables
 const animations= preload("res://animations/sophia_animations.tres")
-
+const responseAnimations=["response1","response2"]
+#"response1","response2","response3"
 
 func play_animation(animation_name:String)->void:
 	animated_sprite_2d.sprite_frames=animations
-	animated_sprite_2d.play(animation_name)
+	if(animation_name=="response"):
+		animated_sprite_2d.play(responseAnimations.pick_random())
+	else:
+		animated_sprite_2d.play(animation_name)
 	
